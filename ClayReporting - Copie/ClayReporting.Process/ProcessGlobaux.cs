d12dc7d41ad400e042d60cb2e098fd82e932358f
Dude <allan.defrenne@viacesi.fr>
2017-01-23 15:10:23 +0100

    modification des données pour le tableau

--- ClayReporting - Copie/ClayReporting.Process/ProcessGlobaux.cs
+++ ClayReporting - Copie/ClayReporting.Process/ProcessGlobaux.cs
@@ -48,6 +48,42 @@ namespace ClayReporting.Process
             return donnees;
         }
 
+        public Dictionary<int, Dictionary<string, dynamic>> ObtenirDonneesTableau(DateTime debutPeriode, DateTime finPeriode)
+        {
+           
+            if (debutPeriode.Equals(new DateTime()) && finPeriode.Equals(new DateTime()))
+            {
+                debutPeriode = Convert.ToDateTime("1/" + DateTime.Now.Month + "/" + DateTime.Now.Year);
+                finPeriode = Convert.ToDateTime(DateTime.DaysInMonth(DateTime.Now.Year, DateTime.Now.Month) + "/" + DateTime.Now.Month + "/" + DateTime.Now.Year);
+            }
+
+            Dictionary<int, Dictionary<string, dynamic>> donnees = new Dictionary<int, Dictionary<string, dynamic>>();
+
+            List<rapport> rapports = new ManageurDA().Rapports.getAllRapportInPeriod(debutPeriode, finPeriode);
+
+            int i = 0;
+            rapports.ForEach(delegate (rapport rapportExp)
+            {
+
+                string dateRapport = rapportExp.DateJour;
+                rapportExp.data.ForEach(delegate (data data)
+                {
+                    Dictionary<string, dynamic> donnee = new Dictionary<string, dynamic>();
+                    donnee.Add("date", dateRapport);
+                    donnee.Add("quality", data.etat.libelle.ToLower());
+                    donnee.Add("performance", data.etat1.libelle.ToLower());
+                    donnee.Add("couleur", data.couleur.libelle);
+                    donnee.Add("composant", data.composant.libelle);
+                    donnee.Add("layout", data.layout);
+                    donnee.Add("lot", data.lot);
+
+                    donnees.Add(i, new Dictionary<string, dynamic>(donnee));
+                    i++;
+                });
+            });
+            return donnees;
+        }
+
         public void ImporterFichier(string cheminDossier)
         {
             
